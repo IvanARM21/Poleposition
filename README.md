854288b4e4102851e221c32bf5cacc6961f54298
noxxUY <itzmamut2@gmail.com>
2024-10-10 00:10:16 -0300

    actualizacion nashei

--- README.md
+++ README.md
@@ -7,3 +7,15 @@
 2. Luego debemos ejecutar PHP ya sea de Xampp o desde una terminal con ````php -S localhost:3000````
 
 3. Comando para correr TailwindCSS ```npx tailwindcss -i ./css/input.css -o ./css/output.css --watch```
+
+# Descargar actualizacion del proyecto
+
+git clone https://github.com/ivanarm21/poleposition.git
+
+# Actualizar
+
+1. ```git add .```
+
+2. ```git commit -m "explicar que se hizo"```
+
+3. ```git push origin main```
--- views/index.php
+++ views/index.php
@@ -60,72 +60,94 @@
 
     <!-- divs autos -->
 
-    <div class="flex flex-col sm:flex-row space-x-4 mx-auto w-max">
+    <div class="flex flex-col sm:flex-row justify-center mx-auto w-full sm:space-x-4">
 
-       <div
-            class="border-2 p-4 rounded-lg shadow-md mt-5 transition-transform transform hover:shadow-lg max-w-full sm:max-w-md md:max-w-lg flex flex-col sm:flex-row w-96 h-full">
-            <a href="/catalogo">
-            <div>
-                <img id="myImage" src="../img/catalogo/m3-competition-1.jpg"
-                    class="w-96 h-96 object-cover transition duration-500 ease-in-out transform hover:scale-105"
-                    onmouseover="this.src='../img/catalogo/m3-competition-2.jpg'"
-                    onmouseout="this.src='../img/catalogo/m3-competition-1.jpg'">
+<div
+    class="hover:border-1 hover:border-red-600 border-2 p-4 rounded-lg shadow-md mt-5 transition-transform transform hover:shadow-lg max-w-full sm:max-w-md md:max-w-lg flex flex-col sm:flex-row w-96 h-full">
+    <a href="/catalogo">
+        <div>
+            <img id="myImage" src="../img/catalogo/m3-competition-1.jpg"
+                class="w-96 h-96 object-cover transition duration-500 ease-in-out transform hover:scale-102"
+                onmouseover="this.src='../img/catalogo/m3-competition-2.jpg'"
+                onmouseout="this.src='../img/catalogo/m3-competition-1.jpg'">
 
-                <h3 class="font-extrabold text-left text-gray-800 mb-2 uppercase text-lg sm:text-xl lg:text-2xl pt-4">
-                    2023 M3 COMPETITION
-                </h3>
-                <div class="border-2 my-4 shadow-2xl"></div>
-                <h3 class="font-extrabold text-left text-green-800 mb-2 uppercase text-lg sm:text-xl lg:text-2xl italic">
-                    US$ 132.855,00
-                </h3>
-            </div>
+            <h3 class="font-extrabold text-left text-gray-800 mb-2 uppercase text-lg sm:text-xl lg:text-2xl pt-4">
+                2023 M3 COMPETITION
+            </h3>
+            <div class="border-2 shadow-2xl"></div>
+            <h3 class="font-extrabold text-left text-green-900 uppercase text-lg sm:text-xl lg:text-xl italic">
+                US$ 132.855,00
+            </h3>
+        </div>
+    </a>
+</div>
+
+<div
+    class="hover:border-1 hover:border-red-600 border-2 p-4 rounded-lg shadow-md mt-5 transition-transform transform hover:shadow-lg max-w-full sm:max-w-md md:max-w-lg flex flex-col sm:flex-row w-96 h-full">
+    <a href="/catalogo">
+        <div>
+            <img id="myImage" src="../img/catalogo/amg-g63-1.jpg"
+                class="w-96 h-96 object-cover transition duration-500 ease-in-out transform hover:scale-102"
+                onmouseover="this.src='../img/catalogo/amg-g63-2.jpg'"
+                onmouseout="this.src='../img/catalogo/amg-g63-1.jpg'">
+
+            <h3 class="font-extrabold text-left text-gray-800 mb-2 uppercase text-lg sm:text-xl lg:text-2xl pt-4">
+                2021 MERCEDES AMG G63   
+            </h3>
+            <div class="border-2 shadow-2xl"></div>
+            <h3 class="font-extrabold text-left text-green-900 uppercase text-lg sm:text-xl lg:text-xl italic">
+                US$ 167.950,00
+            </h3>
+        </div>
+    </a>
+</div>
+
+<div
+    class="hover:border-1 hover:border-red-600 border-2 p-4 rounded-lg shadow-md mt-5 transition-transform transform hover:shadow-lg max-w-full sm:max-w-md md:max-w-lg flex flex-col sm:flex-row w-96 h-full">
+    <a href="/catalogo">
+        <div>
+            <img id="myImage" src="../img/catalogo/cayenne-coupe-1.jpg"
+                class="w-96 h-96 object-cover transition duration-500 ease-in-out transform hover:scale-102"
+                onmouseover="this.src='../img/catalogo/cayenne-coupe-2.jpg'"
+                onmouseout="this.src='../img/catalogo/cayenne-coupe-1.jpg'">
+
+            <h3 class="font-extrabold text-left text-gray-800 mb-2 uppercase text-lg sm:text-xl lg:text-2xl pt-4">
+                2023 Cayenne Coupe
+            </h3>
+            <div class="border-2 shadow-2xl"></div>
+            <h3 class="font-extrabold text-left text-green-900 uppercase text-lg sm:text-xl lg:text-xl italic">
+                US$ 106.096,00
+            </h3>
+        </div>
+    </a> 
+</div>
+</div>
+
+<div class="flex flex-row items-center justify-center sm:justify-normal sm:items-left px-4 sm:px-8 lg:px-28">
+<a name="catalogo"
+                class="uppercase mt-6 mx-auto sm:mx-0 w-fit block py-3 px-8 text-xl text-white bg-red-600 hover:bg-red-700 rounded-lg font-extrabold transition-colors duration-300"
+                href="./catalogo">
+                Cátalogo
             </a>
+</div>
 
-        </div>
-        <!-- 2 -->
+<!-- contactenos -->
 
-        <div
-            class="border-2 p-4 rounded-lg shadow-md mt-5 transition-transform transform hover:shadow-lg max-w-full sm:max-w-md md:max-w-lg flex flex-col sm:flex-row w-96 h-full">
-            <a href="/catalogo">
-            <div>
-                <img id="myImage" src="../img/catalogo/amg-g63-1.jpg"
-                    class="w-96 h-96 object-cover transition duration-500 ease-in-out transform hover:scale-105"
-                    onmouseover="this.src='../img/catalogo/amg-g63-2.jpg'"
-                    onmouseout="this.src='../img/catalogo/amg-g63-1.jpg'">
+<section>
 
-                <h3 class="font-extrabold text-left text-gray-800 mb-2 uppercase text-lg sm:text-xl lg:text-2xl pt-4">
-                    2021 MERCEDES AMG G63   
-                </h3>
-                <div class="border-2 my-4 shadow-2xl"></div>
-                <h3 class="font-extrabold text-left text-green-800 mb-2 uppercase text-lg sm:text-xl lg:text-2xl italic">
-                    US$ 167.950,00
-                </h3>
-            </div>
-            </a>
-        </div>
+    <div>
+    <h2
+        class="pt-20 font-black text-3xl sm:text-4xl lg:text-5xl text-red-600 flex justify-center lg:justify-normal uppercase text-center sm:text-left">
+        Contáctenos</h2>
+        <p
+        class="font-medium text-lg sm:text-xl lg:text-2xl text-gray-600 flex justify-center lg:justify-normal text-center sm:text-left sm:pt-5">
+        Nuestros horarios de atención al cliente:</p>
+        <p
+        class="font-medium text-lg  text-zinc-400 flex justify-center lg:justify-normal text-center sm:text-left">
+        Lunes a Viernes de 8.30 a 12.30 y de 14.00 a 18.30; Sábado de 8.30 a 12:30</p>
 
-        <!-- 3 -->
-
-        <div
-            class="border-2 p-4 rounded-lg shadow-md mt-5 transition-transform transform hover:shadow-lg max-w-full sm:max-w-md md:max-w-lg flex flex-col sm:flex-row w-96 h-full">
-            <a href="/catalogo">
-            <div>
-                <img id="myImage" src="../img/catalogo/cayenne-coupe-1.jpg"
-                    class="w-96 h-96 object-cover transition duration-500 ease-in-out transform hover:scale-105"
-                    onmouseover="this.src='../img/catalogo/cayenne-coupe-2.jpg'"
-                    onmouseout="this.src='../img/catalogo/cayenne-coupe-1.jpg'">
-
-                <h3 class="font-extrabold text-left text-gray-800 mb-2 uppercase text-lg sm:text-xl lg:text-2xl pt-4">
-                    2023 Cayenne Coupe
-                </h3>
-                <div class="border-2 my-4 shadow-2xl"></div>
-                <h3 class="font-extrabold text-left text-green-800 mb-2 uppercase text-lg sm:text-xl lg:text-2xl italic">
-                    US$ 106.096,00
-                </h3>
-            </div>
-            </a> 
-        </div>
-
+        <h2
+        class="pt-5 font-black text-2xl sm:text-3xl lg:text-4xl text-red-600 flex justify-center lg:justify-normal uppercase text-center sm:text-left">
+        (+598) 91 014 226</h2>
     </div>
-
 </section>
