ee39b6473734831e83cbf2f21d224e01c85551f4
noxxUY <itzmamut2@gmail.com>
2024-10-20 18:09:12 -0300

    Se hizo el historial de compras

--- js/perfil.js
+++ js/perfil.js
@@ -1,26 +1,35 @@
 export function loadPerfilButtons() {
     const perfilBtn = document.getElementById('perfilBtn');
     const seguridadBtn = document.getElementById('seguridadBtn');
+    const historialBtn = document.getElementById('historialBtn'); 
+
     const perfilForm = document.getElementById('perfilForm');
     const seguridadForm = document.getElementById('seguridadForm');
+    const historialForm = document.getElementById('historialForm'); 
 
     const cambiarContraseñaBtn = document.getElementById('cambiar-contraseña');
 
-    if(perfilBtn && seguridadBtn) {
+    if(perfilBtn && seguridadBtn && historialBtn) {
         perfilBtn.addEventListener('click', function() {
             perfilForm.classList.remove('hidden');
             seguridadForm.classList.add('hidden');
+            historialForm.classList.add('hidden');
         });
         seguridadBtn.addEventListener('click', function() {
             perfilForm.classList.add('hidden');
             seguridadForm.classList.remove('hidden');
+            historialForm.classList.add('hidden'); 
+        });
+        historialBtn.addEventListener('click', function() {
+            perfilForm.classList.add('hidden');
+            seguridadForm.classList.add('hidden');
+            historialForm.classList.remove('hidden'); 
         });
     }
-    
+
     if(cambiarContraseñaBtn) {
         cambiarContraseñaBtn.addEventListener('click', () => {
-            console.log("Cambiar COntraseña btn")
+            console.log("Cambiar Contraseña btn");
         });
     }
-    
 }
