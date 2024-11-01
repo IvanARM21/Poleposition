60b2035481b63ace66577c46494230146bef1081
Iván <ivanrms371@gmail.com>
2024-10-31 22:47:35 -0300

    Validaciones desde JavaScript y acomdando responsive de la página de comprar

--- js/comprar.js
+++ js/comprar.js
@@ -1,7 +1,7 @@
 import { PAGE_URL, TAX } from "./constants.js";
 import { priceFormatted } from "./helpers.js";
 
-
+const compraForm = document.getElementById("compraForm")
 
 const btnBuy = document.getElementsByName("comprar")[0];
 const btnRent = document.getElementsByName("alquilar")[0];
@@ -21,6 +21,7 @@ const totalElement = document.getElementById('total');
 export const loadBuy = () => {
     btnBuy?.addEventListener("click", loadVehicle);
     btnRent?.addEventListener("click", loadVehicle);
+    compraForm.addEventListener("submit", validateInput);
 }
 
 const loadVehicle = async (e) => {
@@ -100,3 +101,129 @@ const loadVehicleInfo = (vehicle) => {
         confirmBtn.textContent = tipo === "compra" ? "Confirmar compra" : "Confirmar alquiler";
     } 
 };
+
+
+const validateInput = (event) => {
+    event.preventDefault(); // Evitar el envío del formulario
+
+    // Limpiamos mensajes de error previos
+    document.querySelectorAll(".error-message").forEach(el => el.remove());
+
+    // Función para crear mensajes de error
+    function showError(input, message) {
+        if (!input || !message) return
+        const error = document.createElement("div");
+        error.classList.add("error-message", "text-red-600", "text-xs", "flex", "gap-1", "items-center");
+        error.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="min-h-4 min-w-4 size-4">
+<path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clip-rule="evenodd" />
+</svg> <p>${message}</p>`
+            ;
+        input.classList.add("outline-none", "ring-2", "ring-red-400")
+        input.insertAdjacentElement("afterend", error);
+    }
+
+    let isValid = true;
+
+    // Campos para realizar la validación
+    const inputs = {
+        nombre: {
+            value: document.getElementById("nombre").value.trim(),
+            required: true,
+            message: "El nombre es obligatorio."
+        },
+        apellido: {
+            value: document.getElementById("apellido").value.trim(),
+            required: true,
+            message: "El apellido es obligatorio."
+        },
+        email: {
+            value: document.getElementById("email").value.trim(),
+            required: true,
+            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // Patrón de validación de emails
+            message: "Formato de email inválido."
+        },
+        direccion: {
+            value: document.getElementById("direccion").value.trim(),
+            required: true,
+            message: "La dirección es obligatoria."
+        },
+        ciudad: {
+            value: document.getElementById("ciudad").value.trim(),
+            required: true,
+            message: "La ciudad es obligatoria."
+        },
+        codigo: {
+            value: document.getElementById("codigo").value.trim(),
+            required: true,
+            pattern: /^[0-9]{5}$/, // Patrón de valicación para el postal
+            message: "El código postal debe tener 5 dígitos."
+        },
+        pais: {
+            value: document.getElementById("pais").value.trim(),
+            required: true,
+            message: "El país es obligatorio."
+        },
+        telefono: {
+            value: document.getElementById("telefono").value.trim(),
+            required: true,
+            pattern: /^[0-9]{9}$/,
+            message: "El teléfono debe tener 9 dígitos."
+        },
+        tarjeta: {
+            value: document.getElementById("tarjeta").value.trim(),
+            required: true,
+            pattern: /^[0-9]{16}$/,
+            message: "El número de tarjeta debe tener 16 dígitos."
+        },
+        nombreTarjeta: {
+            value: document.getElementById("nombreTarjeta").value.trim(),
+            required: true,
+            message: "El nombre en la tarjeta es obligatorio."
+        },
+        caducidad: {
+            value: document.getElementById("caducidad").value.trim(),
+            required: true,
+            pattern: /^(0[1-9]|1[0-2])\/?([0-9]{2})$/, // Formato de gvalidación para la caducidad de la tarjeta
+            message: "Formato de caducidad inválido (MM/AA)."
+        },
+        cvc: {
+            value: document.getElementById("cvc").value.trim(),
+            required: true,
+            pattern: /^[0-9]{3,5}$/, // Validacion para CVC
+            message: "El CVC debe tener entre 3 y 5 dígitos."
+        }
+    };
+
+    // Esté código iterará sobre cada input  para saber si mostrar alerta o no
+    for (const [key, { value, required, pattern, message }] of Object.entries(inputs)) {
+        const inputEl = document.getElementById(key);
+
+        if (required && !value) {
+            showError(inputEl, message);
+            isValid = false;
+        } else if (pattern && !pattern.test(value)) {
+            showError(inputEl, message);
+            isValid = false;
+        }
+    }
+
+    // Validación de fecha de caducidad
+    const caducidadInput = document.getElementById("caducidad");
+    const [mes, anio] = inputs.caducidad.value.split('/');
+    const caducidad = new Date(`20${anio}`, mes, 0); // Último día del mes de caducidad
+
+    if (caducidad < new Date()) {
+        showError(caducidadInput, "La fecha de caducidad está vencida.");
+        isValid = false;
+    }
+
+    if (isValid) {
+
+        // Obtener datos de localstorage
+
+        // Llamar api de php con todos los datos, habrá otra validación desde php
+
+
+
+    }
+}
--- views/comprar/index.php
+++ views/comprar/index.php
@@ -1,5 +1,5 @@
 <section class="max-w-screen-xl mx-auto">
-    <form method="POST" action="">
+    <form method="POST" action="" id="compraForm">
         <div class="flex flex-col-reverse lg:grid lg:grid-cols-2 px-6 gap-10">
             <div class="rounded-xl">
                 <div>
@@ -7,29 +7,35 @@
 
                     <div class="flex flex-col gap-1 mt-5">
                         <label for="email" class="text-lg text-gray-700">Email</label>
-                        <input id="email" name="email" type="email" class="w-full py-2 px-4 border rounded-md" value="<?= htmlspecialchars($email) ?>">
+                        <input id="email" name="email" type="email" class="w-full py-2 px-4 border rounded-md"
+                            value="<?= htmlspecialchars($email) ?>">
                         <?php if (!empty($errors['email'])): ?>
-                            <p class="text-red-600"><?= htmlspecialchars($errors['email']) ?></p> 
+                            <p class="text-red-600"><?= htmlspecialchars($errors['email']) ?></p>
                         <?php endif; ?>
                     </div>
                 </div>
 
-                <div class="mt-2 pt-2 border-t">
+                <div class="mt-8 pt-8 border-t">
                     <h2 class="text-xl font-medium text-gray-800">Información de cliente</h2>
 
                     <div class="flex flex-col gap-3 mt-2">
                         <div class="grid grid-cols-2 gap-2">
                             <div class="flex flex-col gap-1">
-                                <label for="nombre" class="text-lg text-gray-700">Nombre <span class="text-red-600">*</span> </label>
-                                <input type="text" id="nombre" name="nombre" class="w-full py-2 px-4 border rounded-md" value="<?= htmlspecialchars($nombre) ?>">
+                                <label for="nombre" class="text-lg text-gray-700">Nombre <span
+                                        class="text-red-600">*</span> </label>
+                                <input type="text" id="nombre" name="nombre" class="w-full py-2 px-4 border rounded-md"
+                                    value="<?= htmlspecialchars($nombre) ?>">
                                 <?php if (!empty($errors['nombre'])): ?>
                                     <p class="text-red-600"><?= htmlspecialchars($errors['nombre']) ?></p>
                                 <?php endif; ?>
                             </div>
 
                             <div class="flex flex-col gap-1">
-                                <label for="apellido" class="text-lg text-gray-700">Apellido <span class="text-red-600">*</span> </label>
-                                <input type="text" id="apellido" name="apellido" class="w-full py-2 px-4 border rounded-md" value="<?= htmlspecialchars($apellido) ?>">
+                                <label for="apellido" class="text-lg text-gray-700">Apellido <span
+                                        class="text-red-600">*</span> </label>
+                                <input type="text" id="apellido" name="apellido"
+                                    class="w-full py-2 px-4 border rounded-md"
+                                    value="<?= htmlspecialchars($apellido) ?>">
                                 <?php if (!empty($errors['apellido'])): ?>
                                     <p class="text-red-600"><?= htmlspecialchars($errors['apellido']) ?></p>
                                 <?php endif; ?>
@@ -37,8 +43,10 @@
                         </div>
 
                         <div class="flex flex-col gap-1">
-                            <label for="direccion" class="text-lg text-gray-700">Dirección <span class="text-red-600">*</span> </label>
-                            <input type="text" id="direccion" name="direccion" class="w-full py-2 px-4 border rounded-md">
+                            <label for="direccion" class="text-lg text-gray-700">Dirección <span
+                                    class="text-red-600">*</span> </label>
+                            <input type="text" id="direccion" name="direccion"
+                                class="w-full py-2 px-4 border rounded-md">
                             <?php if (!empty($errors['direccion'])): ?>
                                 <p class="text-red-600"><?= htmlspecialchars($errors['direccion']) ?></p>
                             <?php endif; ?>
@@ -46,7 +54,8 @@
 
                         <div class="flex flex-col gap-1">
                             <label for="apartamento" class="text-lg text-gray-700">Apartamento, suite, etc..</label>
-                            <input type="text" id="apartamento" name="apartamento" class="w-full py-2 px-4 border rounded-md">
+                            <input type="text" id="apartamento" name="apartamento"
+                                class="w-full py-2 px-4 border rounded-md">
                             <?php if (!empty($errors['apartamento'])): ?>
                                 <p class="text-red-600"><?= htmlspecialchars($errors['apartamento']) ?></p>
                             <?php endif; ?>
@@ -54,21 +63,24 @@
 
                         <div class="grid grid-cols-3 gap-2">
                             <div class="flex flex-col gap-1">
-                                <label for="ciudad" class="text-lg text-gray-700">Ciudad <span class="text-red-600">*</span> </label>
+                                <label for="ciudad" class="text-lg text-gray-700">Ciudad <span
+                                        class="text-red-600">*</span> </label>
                                 <input type="text" id="ciudad" name="ciudad" class="w-full py-2 px-4 border rounded-md">
                                 <?php if (!empty($errors['ciudad'])): ?>
                                     <p class="text-red-600"><?= htmlspecialchars($errors['ciudad']) ?></p>
                                 <?php endif; ?>
                             </div>
                             <div class="flex flex-col gap-1">
-                                <label for="codigo" class="text-lg text-gray-700">Código Postal <span class="text-red-600">*</span> </label>
+                                <label for="codigo" class="text-lg text-gray-700">Código Postal <span
+                                        class="text-red-600">*</span> </label>
                                 <input type="text" id="codigo" name="codigo" class="w-full py-2 px-4 border rounded-md">
                                 <?php if (!empty($errors['codigo'])): ?>
                                     <p class="text-red-600"><?= htmlspecialchars($errors['codigo']) ?></p>
                                 <?php endif; ?>
                             </div>
                             <div class="flex flex-col gap-1">
-                                <label for="pais" class="text-lg text-gray-700">País <span class="text-red-600">*</span> </label>
+                                <label for="pais" class="text-lg text-gray-700">País <span class="text-red-600">*</span>
+                                </label>
                                 <input type="text" id="pais" name="pais" class="w-full py-2 px-4 border rounded-md">
                                 <?php if (!empty($errors['pais'])): ?>
                                     <p class="text-red-600"><?= htmlspecialchars($errors['pais']) ?></p>
@@ -77,7 +89,8 @@
                         </div>
 
                         <div class="flex flex-col gap-1">
-                            <label for="telefono" class="text-lg text-gray-700">Teléfono <span class="text-red-600">*</span> </label>
+                            <label for="telefono" class="text-lg text-gray-700">Teléfono <span
+                                    class="text-red-600">*</span> </label>
                             <input type="text" id="telefono" name="telefono" class="w-full py-2 px-4 border rounded-md">
                             <?php if (!empty($errors['telefono'])): ?>
                                 <p class="text-red-600"><?= htmlspecialchars($errors['telefono']) ?></p>
@@ -85,35 +98,42 @@
                         </div>
                     </div>
 
-                    <div class="mt-2 pt-2 border-t">
+                    <div class="mt-8 pt-8 border-t">
                         <h2 class="text-xl font-medium text-gray-800">Pago</h2>
 
                         <div class="flex flex-col gap-3 mt-2">
                             <div class="flex flex-col gap-1">
-                                <label for="tarjeta" class="text-lg text-gray-700">Número de Tarjeta <span class="text-red-600">*</span> </label>
-                                <input type="text" id="tarjeta" name="tarjeta" class="w-full py-2 px-4 border rounded-md">
+                                <label for="tarjeta" class="text-lg text-gray-700">Número de Tarjeta <span
+                                        class="text-red-600">*</span> </label>
+                                <input type="text" id="tarjeta" name="tarjeta"
+                                    class="w-full py-2 px-4 border rounded-md">
                                 <?php if (!empty($errors['tarjeta'])): ?>
                                     <p class="text-red-600"><?= htmlspecialchars($errors['tarjeta']) ?></p>
                                 <?php endif; ?>
                             </div>
                             <div class="flex flex-col gap-1">
-                                <label for="nombreTarjeta" class="text-lg text-gray-700">Nombre en Tarjeta <span class="text-red-600">*</span> </label>
-                                <input type="text" id="nombreTarjeta" name="nombreTarjeta" class="w-full py-2 px-4 border rounded-md">
+                                <label for="nombreTarjeta" class="text-lg text-gray-700">Nombre en Tarjeta <span
+                                        class="text-red-600">*</span> </label>
+                                <input type="text" id="nombreTarjeta" name="nombreTarjeta"
+                                    class="w-full py-2 px-4 border rounded-md">
                                 <?php if (!empty($errors['nombreTarjeta'])): ?>
                                     <p class="text-red-600"><?= htmlspecialchars($errors['nombreTarjeta']) ?></p>
                                 <?php endif; ?>
                             </div>
 
-                            <div class="grid grid-cols-4 gap-2">
-                                <div class="flex flex-col gap-1 col-span-3">
-                                    <label for="caducidad" class="text-lg text-gray-700">Fecha de Caducidad (MM/AA) <span class="text-red-600">*</span> </label>
-                                    <input type="text" id="caducidad" name="caducidad" class="w-full py-2 px-4 border rounded-md">
+                            <div class="grid grid-cols-6 gap-2">
+                                <div class="flex flex-col gap-1 col-span-4">
+                                    <label for="caducidad" class="text-lg text-gray-700">Fecha de Caducidad (MM/AA)
+                                        <span class="text-red-600">*</span> </label>
+                                    <input type="text" id="caducidad" name="caducidad"
+                                        class="w-full py-2 px-4 border rounded-md">
                                     <?php if (!empty($errors['caducidad'])): ?>
                                         <p class="text-red-600"><?= htmlspecialchars($errors['caducidad']) ?></p>
                                     <?php endif; ?>
                                 </div>
-                                <div class="flex flex-col gap-1 col-span-1">
-                                    <label for="cvc" class="text-lg text-gray-700">CVC <span class="text-red-600">*</span> </label>
+                                <div class="flex flex-col gap-1 col-span-2">
+                                    <label for="cvc" class="text-lg text-gray-700">CVC <span
+                                            class="text-red-600">*</span> </label>
                                     <input type="text" id="cvc" name="cvc" class="w-full py-2 px-4 border rounded-md">
                                     <?php if (!empty($errors['cvc'])): ?>
                                         <p class="text-red-600"><?= htmlspecialchars($errors['cvc']) ?></p>
@@ -122,32 +142,37 @@
                             </div>
                         </div>
                     </div>
+                    <button type="submit" id="confirmBtn"
+                        class="bg-red-600 font-medium text-lg w-full py-2 px-4 text-white rounded-xl border-t mt-8 lg:hidden">
+                        Confirmar compra
+                    </button>
                 </div>
             </div>
-            <div class="flex flex-col gap-5 lg:sticky top-5 h-[581px]">
+            <div class="flex flex-col gap-5 lg:sticky top-5 lg:h-[581px]">
                 <h2 class="text-xl font-medium text-gray-800">Resumen compra</h2>
 
                 <div class="flex flex-col gap-10">
-                    <div class="flex justify-between border-y py-8">
-                        <div class="flex gap-5">
-                            <img id="imagen" alt="" src="" class="h-32 w-48 object-cover rounded-xl">
+                    <div class="flex  sm:flex-row justify-between border-y py-8">
+                        <div class="flex gap-2 sm:gap-5">
+                            <img id="imagen" alt="" src="" class="h-18 sm:h-32 w-28 sm:w-48 object-cover rounded-xl">
 
-                            <div class="flex flex-col justify-between py-2">
+                            <div class="flex flex-col justify-between py-1">
                                 <div>
-                                    <h3 class="text-xl text-gray-800 hover:text-red-600 font-medium" id="titulo">
+                                    <h3 class="text-sm sm:text-xl text-gray-800 hover:text-red-600 font-medium"
+                                        id="titulo">
                                         Mercedes Benz AMG G63
                                     </h3>
                                     <div class="flex gap-2">
-                                        <p class="text-gray-500 font-medium" id="año"></p>
-                                        <p class="text-gray-500 font-medium">-</p>
-                                        <p class="text-gray-500 font-medium" id="km"></p>
+                                        <p class="text-gray-500 font-medium text-xs sm:text-base" id="año"></p>
+                                        <p class="text-gray-500 font-medium text-xs sm:text-base">-</p>
+                                        <p class="text-gray-500 font-medium text-xs sm:text-base" id="km"></p>
                                     </div>
                                 </div>
 
-                                <p class="text-lg text-gray-500 font-medium" id="precio"></p>
+                                <p class="text-sm sm:text-base text-gray-500 font-medium " id="precio"></p>
                             </div>
                         </div>
-                        <p class="text-gray-500 font-medium py-2">
+                        <p class="text-gray-500 font-medium py-2 text-xs sm:text-base text-nowrap">
                             Tipo:
                             <span class="py-1 px-2 rounded-lg capitalize" id="tipo">Alquiler</span>
                         </p>
@@ -169,7 +194,8 @@
                             <p class="text-xl text-gray-700 font-medium" id="total">USD 533.55</p>
                         </div>
 
-                        <button type="submit" id="confirmBtn" class="bg-red-600 font-medium text-lg w-full py-2 px-4 text-white rounded-xl border-t mt-4">
+                        <button type="submit" id="confirmBtn"
+                            class="bg-red-600 font-medium text-lg w-full py-2 px-4 text-white rounded-xl border-t mt-4 hidden lg:block">
                             Confirmar compra
                         </button>
                     </div>
